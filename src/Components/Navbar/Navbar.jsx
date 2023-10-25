9178c7738b5e02475de1ceeb9498f29d6154a34b
Lalit Jakhar <lalit25jakhar@email.com>
2023-10-25 12:51:00 +0530

    Initial commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import { Box, Container } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import Navbarlogo from "../../Assets/Navlogo.webp";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
-    return (
-        <>
-        
-        </>
-    );
-}
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#300101" }}>
+      <Container>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <img src={Navbarlogo} height={40} />
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton onClick={handleOpenNavMenu} color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorElNav}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem>
+                <Button color="inherit">Home</Button>
+              </MenuItem>
+              <MenuItem>
+                <Button color="inherit">Our Brands </Button>
+              </MenuItem>
+              <MenuItem>
+                <Button color="inherit">Products</Button>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: {
+                xs: "none",
+                md: "flex",
+                display: "flex",
+                justifyContent: "end",
+              },
+            }}
+          >
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">Our Brands </Button>
+            <Button color="inherit">Products</Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default Navbar;
